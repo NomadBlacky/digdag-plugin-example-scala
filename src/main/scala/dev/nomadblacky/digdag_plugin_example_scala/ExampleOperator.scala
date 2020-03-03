3a0f5e293a509ced18c56a1ce2362bd9d4b43d51
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2020-03-04 00:37:31 +0900

    An example of getting parameters

--- src/main/scala/dev/nomadblacky/digdag_plugin_example_scala/ExampleOperator.scala
+++ src/main/scala/dev/nomadblacky/digdag_plugin_example_scala/ExampleOperator.scala
@@ -1,16 +1,25 @@
 package dev.nomadblacky.digdag_plugin_example_scala
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.digdag.spi.{Operator, OperatorContext, OperatorFactory, TaskResult}
 import io.digdag.util.BaseOperator
 import org.slf4j.LoggerFactory
+
+import scala.jdk.CollectionConverters._
 
 class ExampleOperator(_context: OperatorContext) extends BaseOperator(_context) {
 
   private[this] val logger = LoggerFactory.getLogger(classOf[ExampleOperator])
 
   override def runTask(): TaskResult = {
-    logger.info("Hello, Digdag!")
-    TaskResult.empty(request)
+    val config = this.request.getConfig
+    val pretty = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(config)
+    logger.info(pretty)
+
+    logger.info("key = {}", config.get("key", classOf[String]))
+    logger.info("array = {}", config.getList("array", classOf[Int]).asScala.toSeq)
+
+    TaskResult.empty(this.request)
   }
 }
 
